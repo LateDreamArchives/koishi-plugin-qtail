e7c8caa85cdc68d219dc2aabb461bb6791689ecb
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-09-19 19:04:03 +0800

    chore: add config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {Context, Schema} from 'koishi';
-import qtail from 'qtail-js';
+import Qtail_ from 'qtail-js';
 
 export const name = 'qtail';
 export const usage = `
@@ -11,18 +11,28 @@ export const usage = `
 - \`qt <nick:string> <tail:string>\`
 
 栗子:
-- qtail 迟梦 喵~
+- qtail 晚梦 喵~
 - qt Love-kogasa ~
 `;
-export const Config = Schema.object({});
-export const apply = (ctx: Context) => {
+
+interface Config {
+	compatibleMode: boolean;
+}
+
+export const Config: Schema<Config> = Schema.intersect([
+	Schema.object({
+		compatibleMode: Schema.boolean().default(true).description('兼容模式'),
+	}).description('一般配置')
+]);
+export const apply = (ctx: Context, cfg: Config) => {
 	ctx.command('qtail <nick:string> <tail:string>', '生成带有小尾巴的昵称')
 		.alias('qt')
 		.action((_, nick, tail) => {
 			try {
-				const Qtail = new qtail(nick, tail, true);
-				const result = Qtail.generate();
-				_.session?.send(result);
-			} catch(e: any) {_.session?.send(e.message)}
+				const qtail = new Qtail_(nick, tail, cfg.compatibleMode);
+				_.session?.send(qtail.gen());
+			} catch(e: any) {_.session?.send(`出错惹 qwq\n${e.message}`)}
 		});
+
+	ctx.set('qtail', (nick: string, tail: string, compatible: boolean) => (new Qtail_(nick, tail, compatible)).gen());
 }
